28e2cd5add49422e5d219cb16d6a2a0b3abd63b3
Julius Schmidt <newceptiondev@googlemail.com>
2024-09-07 18:34:06 +0200

    design landing page

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,19 +1,23 @@
 "use client";
 
+import Image from "next/image";
 import { createLobby, joinLobby } from "./actions";
 import ThemeSwitcher from "./themeswitcher";
 
 export default function Home() {
   return (
-    <main className="h-screen landingPage">
-      <div className="flex flex-row justify-evenly items-center h-[10vh]">
+    <main className="h-screen">
+      <div className="flex flex-row justify-evenly items-center h-[30vh]">
         <div className="w-1/5 m-8"></div>
-        <h1 className="text-5xl w-3/5 text-center">Planning Poker</h1>
-        <div className="w-1/5 flex justify-end m-8">
+        <div className="w-3/5 flex flex-col items-center justify-center">
+          <Image src="/logo_large.png" width={100} height={100} alt="logo" />
+          <h1 className="text-6xl text-center">Planning<br />Poker</h1>
+        </div>
+        <div className="w-1/5 flex justify-end m-8 self-start">
           <ThemeSwitcher />
         </div>
       </div>
-      <div id="content" className="flex flex-col justify-center align-middle h-[88vh]">
+      <div id="content" className="flex flex-col justify-center align-middle h-[66vh]">
 
         <div id="lobby-join" className="flex flex-col items-center">
           <h1 className="mb-4">Enter a Lobby Code</h1>
@@ -22,7 +26,7 @@ export default function Home() {
             <button id="lobby-join-button" className="btn" type="submit">Join</button>
           </form>
         </div>
-        <div id="spacer" className="bg-white w-1/2 h-0.5 self-center m-4">
+        <div id="spacer" className="w-1/3 h-0.5 self-center m-4 secondary">
         </div>
         <div id="lobby-create" className="flex flex-col items-center">
           <h1 className="mb-4">Create a new Lobby</h1>
